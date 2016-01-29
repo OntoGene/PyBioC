a0fe375f7934100506fab48b2a60d870503f9ffe
Lenz <Lenz.Furrer@gmail.com>
2016-01-29 11:28:31 +0100

    python 2to3 encoding stuff

--- src/bioc/bioc_writer.py
+++ src/bioc/bioc_writer.py
@@ -4,78 +4,82 @@ from lxml.builder import E
 from lxml.etree import tostring
 
 class BioCWriter:
-    
+
     def __init__(self, filename=None, collection=None):
-        
+
         self.root_tree = None
-                        
+
         self.collection = None
         self.doctype = '''<?xml version='1.0' encoding='UTF-8'?>'''
         self.doctype += '''<!DOCTYPE collection SYSTEM 'BioC.dtd'>'''
         self.filename = filename
-        
+
         if collection is not None:
             self.collection = collection
-        
+
         if filename is not None:
             self.filename = filename
-        
+
     def __str__(self):
         """ A BioCWriter object can be printed as string.
         """
         self._check_for_data()
-            
+
         self.build()
-        s = tostring(self.root_tree, 
-                    pretty_print=True, 
-                    doctype=self.doctype)
-                    
+
+        # Use `encoding=str` to avoid returning a byte object.
+        # Encode the string when writing to file.
+        # Set the encoding declaration by using self.doctype.
+        s = tostring(self.root_tree,
+                     encoding=str,
+                     pretty_print=True,
+                     doctype=self.doctype)
+
         return s
-    
+
     def _check_for_data(self):
         if self.collection is None:
             raise(Exception('No data available.'))
-    
+
     def write(self, filename=None):
         """ Use this method to write the data in the PyBioC objects
             to disk.
-            
+
             filename:   Output file path (optional argument; filename
                         provided by __init__ used otherwise.)
         """
-        if filename is not None:
-            self.filename = filename
-        
-        if self.filename is None:
-            raise(Exception('No output file path provided.'))
-            
-        f = open(self.filename, 'w')
-        f.write(self.__str__())
-        
+        if filename is None:
+            if self.filename is None:
+                raise(Exception('No output file path provided.'))
+            filename = self.filename
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(str(self))
+
     def build(self):
         self._build_collection()
-        
+
     def _build_collection(self):
-        self.root_tree = E('collection', 
+        self.root_tree = E('collection',
                             E('source'), E('date'), E('key'))
         self.root_tree.xpath('source')[0].text = self.collection.source
         self.root_tree.xpath('date')[0].text = self.collection.date
-        self.root_tree.xpath('key')[0].text = self.collection.key         
+        self.root_tree.xpath('key')[0].text = self.collection.key
         collection_elem = self.root_tree.xpath('/collection')[0]
         # infon*
         self._build_infons(self.collection.infons, collection_elem)
         # document+
-        self._build_documents(self.collection.documents, 
+        self._build_documents(self.collection.documents,
                                 collection_elem)
-        
+
     def _build_infons(self, infons_dict, infons_parent_elem):
         for infon_key, infon_val in infons_dict.items():
             infons_parent_elem.append(E('infon'))
             infon_elem = infons_parent_elem.xpath('infon')[-1]
-            
+
             infon_elem.attrib['key'] = infon_key
             infon_elem.text = infon_val
-            
+
     def _build_documents(self, documents_list, collection_parent_elem):
         for document in documents_list:
             collection_parent_elem.append(E('document', E('id')))
@@ -89,7 +93,7 @@ class BioCWriter:
             self._build_passages(document.passages, document_elem)
             # relation*
             self._build_relations(document.relations, document_elem)
-            
+
     def _build_passages(self, passages_list, document_parent_elem):
         for passage in passages_list:
             document_parent_elem.append(E('passage'))
@@ -106,11 +110,11 @@ class BioCWriter:
                 # text?, annotation*
                 passage_elem.append(E('text'))
                 passage_elem.xpath('text')[0].text = passage.text
-                self._build_annotations(passage.annotations, 
+                self._build_annotations(passage.annotations,
                                         passage_elem)
             # relation*
             self._build_relations(passage.relations, passage_elem)
-        
+
     def _build_relations(self, relations_list, relations_parent_elem):
         for relation in relations_list:
             relations_parent_elem.append(E('relation'))
@@ -126,9 +130,9 @@ class BioCWriter:
             # id (just #IMPLIED)
             if len(relation.id) > 0:
                 relation_elem.attrib['id'] = relation.id
-        
-    def _build_annotations(self, annotations_list, 
-                            annotations_parent_elem):
+
+    def _build_annotations(self, annotations_list,
+                           annotations_parent_elem):
         for annotation in annotations_list:
             annotations_parent_elem.append(E('annotation'))
             annotation_elem = \
